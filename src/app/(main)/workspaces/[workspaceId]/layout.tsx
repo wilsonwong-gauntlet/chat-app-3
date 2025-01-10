7344fcc41067c9e30bfb5db5c30514e64f3b0a21
Gauntlet <gauntlet@macbookpro.myfiosgateway.com>
2025-01-10 11:56:47 -0500

    minor UI

--- src/app/(main)/workspaces/[workspaceId]/layout.tsx
+++ src/app/(main)/workspaces/[workspaceId]/layout.tsx
@@ -26,7 +26,7 @@ export default async function WorkspaceLayout({
         <div className="hidden md:flex w-60 z-20 flex-col fixed inset-y-0 left-[72px]">
           <WorkspaceSidebarClient />
         </div>
-        <main className="flex-1 h-full pl-[332px]">
+        <main className="flex-1 h-full pl-60">
           {children}
         </main>
       </div>
